30247f3f5593c70bb819cf8fbff68bf5268e1c24
DhruvParmar051 <dhruvparmar051@outlook.com>
2026-02-07 17:42:29 +0530

    update

--- frontend/app.py
+++ frontend/app.py
@@ -52,6 +52,13 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    [data-testid="stToolbar"] {display: none;}
+    </style>
+""", unsafe_allow_html=True)
 # ======================================================
 # HELPERS
 # ======================================================
